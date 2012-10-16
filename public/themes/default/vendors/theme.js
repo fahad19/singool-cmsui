aa22ee0682ce8c54ac8e913537efb212121e0608
Fahad Ibnay Heylaal <fahad19@gmail.com>
2012-10-16 15:10:25 +0100

    toggle nested menu.

--- public/themes/default/vendors/theme.js
+++ public/themes/default/vendors/theme.js
@@ -1,1 +1,16 @@
 console.log('from theme');
+
+$(document).ready(function() {
+	$('aside ul li a').live('click', function() {
+		var siblingUl = $(this).parent().find('ul');
+		if (siblingUl.length > 0) {
+			if (siblingUl.hasClass('open')) {
+				siblingUl.removeClass('open');
+			} else {
+				siblingUl.addClass('open');
+			}
+
+			return false;
+		}
+	});
+});
