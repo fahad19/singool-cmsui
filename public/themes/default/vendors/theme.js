b3e21d93cf868179d3fcc6ee3398d68fdb3420ec
Fahad Ibnay Heylaal <fahad19@gmail.com>
2013-03-23 21:51:49 +0600

    fontawesome and theme's js

--- public/themes/default/vendors/theme.js
+++ public/themes/default/vendors/theme.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-	$('aside ul li a').live('click', function() {
-		var parentLi = $(this).parents('li');
-		var siblingUl = parentLi.find('ul');
-		if (siblingUl.length > 0) {
-			if (parentLi.hasClass('open')) {
-				parentLi.removeClass('open');
-			} else {
-				parentLi.addClass('open');
-			}
+	window.setTimeout(function() {
+		$('aside ul li a').click(function() {
+			var parentLi = $(this).parents('li');
+			var siblingUl = parentLi.find('ul');
+			if (siblingUl.length > 0) {
+				if (parentLi.hasClass('open')) {
+					parentLi.removeClass('open');
+				} else {
+					parentLi.addClass('open');
+				}
 
-			if ($(this).attr('href') == '#') {
-				return false;
+				if ($(this).attr('href') == '#') {
+					return false;
+				}
 			}
-		}
-	});
+		});
+	}, 1000);
 });
